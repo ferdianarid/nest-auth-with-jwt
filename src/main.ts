fbc35d1fea0a45d2be100b6989bcae06c5b0944e
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-03-11 01:16:08 +0700

    enable cors, activate cookie parser and inject jwt

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as cookieParser from "cookie-parser"
+import { CLIENT_URL, PORT } from './config';
 
 async function bootstrap() {
   const NestApp = await NestFactory.create<NestExpressApplication>(AppModule);
-  await NestApp.listen(3000);
+
+  NestApp.use(cookieParser())
+
+  NestApp.enableCors({
+    origin: CLIENT_URL,
+    credentials: true
+  })
+
+  await NestApp.listen(PORT);
 }
 
 bootstrap();
