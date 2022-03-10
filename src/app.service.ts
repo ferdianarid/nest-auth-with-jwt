e705e7f5536f09249669d212ebf8ea43c8092b77
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-03-11 01:15:18 +0700

    update service user

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entity/user.entity';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@InjectRepository(User) private readonly _userRepository: Repository<User>) { }
+
+  async CreateUser(data: any): Promise<User> {
+    return this._userRepository.save(data)
+  }
+
+  async findOneUser(condition: any): Promise<User> {
+    return this._userRepository.findOne(condition)
   }
 }
