a9a8fdfdfdb4cf37201aa8958d2e5986e34cccb7
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-03-11 01:14:47 +0700

    update config typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,30 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_TYPE, DB_USERNAME, SECRET_KEY, TOKEN_EXPIRES } from './config';
 import { User } from './entity/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: parseInt(process.env.DB_PORT),
-      username: 'root',
-      password: '',
-      database: 'nest-auth',
+      host: DB_HOST,
+      port: 3306,
+      username: DB_USERNAME,
+      password: DB_PASSWORD,
+      database: DB_NAME,
       entities: [User],
       synchronize: true,
     }),
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: SECRET_KEY,
+      signOptions: {
+        expiresIn: TOKEN_EXPIRES
+      }
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
