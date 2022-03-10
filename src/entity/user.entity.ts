501199c0f6c4928d60d000064b1f720c7e807183
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-03-11 01:16:34 +0700

    create user entity

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,8 +1,8 @@
-import { Column, Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity("user")
 export class User {
-   @PrimaryColumn()
+   @PrimaryGeneratedColumn()
    id: number
 
    @Column()
